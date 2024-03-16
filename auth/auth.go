8ce83e1d0b4f3e5c3ef710e53254c674c96ce318
sameer-gits <ms3054825@gmail.com>
2024-03-16 23:23:48 +0530

    added session

--- auth/auth.go
+++ auth/auth.go
@@ -11,6 +11,8 @@ import (
 	"github.com/markbates/goth/providers/github"
 )
 
+var store *sessions.CookieStore
+
 func Auth() {
 	err := godotenv.Load()
 	if err != nil {
@@ -21,11 +23,11 @@ func Auth() {
 	githubKey := os.Getenv("GITHUB_KEY")
 	githubSecret := os.Getenv("GITHUB_SECRET")
 
-	key := secretKey       // Replace with your SESSION_SECRET or similar
-	maxAge := 86400 * 30   // 30 days
-	isProd := false        // Set to true when serving over HTTPS
+	key := secretKey     // Replace with your SESSION_SECRET or similar
+	maxAge := 86400 * 30 // 30 days
+	isProd := false      // Set to true when serving over HTTPS
 
-	store := sessions.NewCookieStore([]byte(key))
+	store = sessions.NewCookieStore([]byte(key))
 	store.MaxAge(maxAge)
 	store.Options.Path = "/"
 	store.Options.HttpOnly = true // HttpOnly should always be enabled
--- routes/routes.go
+++ routes/routes.go
@@ -8,11 +8,13 @@ import (
 	"sort"
 
 	"github.com/gorilla/pat"
+	"github.com/gorilla/sessions" // Import Gorilla sessions package
 	"github.com/markbates/goth/gothic"
 )
 
-func AuthCallbackHandler() {
+var store = sessions.NewCookieStore([]byte("your-secret-key")) // Replace "your-secret-key" with your secret key
 
+func AuthCallbackHandler() {
 	m := map[string]string{
 		"github": "Github",
 	}
@@ -25,25 +27,34 @@ func AuthCallbackHandler() {
 	providerIndex := &ProviderIndex{Providers: keys, ProvidersMap: m}
 
 	p := pat.New()
-	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
 
+	p.Get("/auth/{provider}/callback", func(res http.ResponseWriter, req *http.Request) {
 		user, err := gothic.CompleteUserAuth(res, req)
 		if err != nil {
 			fmt.Fprintln(res, err)
 			return
 		}
+
+		session, _ := store.Get(req, "go-session")
+		session.Values["user_id"] = user.UserID
+		session.Save(req, res)
+
 		t, _ := template.New("foo").Parse(userTemplate)
 		t.Execute(res, user)
 	})
 
 	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
 		gothic.Logout(res, req)
+
+		session, _ := store.Get(req, "go-session")
+		delete(session.Values, "user_id")
+		session.Save(req, res)
+
 		res.Header().Set("Location", "/")
 		res.WriteHeader(http.StatusTemporaryRedirect)
 	})
 
 	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-		// try to get the user without re-authenticating
 		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
 			t, _ := template.New("foo").Parse(userTemplate)
 			t.Execute(res, gothUser)
@@ -51,9 +62,17 @@ func AuthCallbackHandler() {
 			gothic.BeginAuthHandler(res, req)
 		}
 	})
+
 	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		session, _ := store.Get(req, "go-session")
+		_, loggedIn := session.Values["user_id"]
+
 		t, _ := template.New("foo").Parse(indexTemplate)
-		t.Execute(res, providerIndex)
+		t.Execute(res, struct {
+			ProviderIndex *ProviderIndex
+			LoggedIn      bool
+		}{providerIndex, loggedIn})
+
 	})
 
 	log.Println("listening on localhost:3000")
@@ -65,9 +84,14 @@ type ProviderIndex struct {
 	ProvidersMap map[string]string
 }
 
-var indexTemplate = `{{range $key,$value:=.Providers}}
-    <p><a href="/auth/{{$value}}">Log in with {{index $.ProvidersMap $value}}</a></p>
-{{end}}`
+var indexTemplate = `{{range $key,$value:=.ProviderIndex.Providers}}
+<p><a href="/auth/{{$value}}">Log in with {{index $.ProviderIndex.ProvidersMap $value}}</a></p>
+{{end}}
+{{if .LoggedIn}}
+<p>Welcome, {{.UserName}}!</p>
+<p><a href="/logout/github">Logout</a></p>
+{{end}}
+`
 
 var userTemplate = `
 <p><a href="/logout/{{.Provider}}">logout</a></p>
