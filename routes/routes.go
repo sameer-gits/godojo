514d1c831537f15e649fec55b767a44d8ce73729
sameer-gits <ms3054825@gmail.com>
2024-03-17 19:53:19 +0530

    auth is completed now

--- routes/routes.go
+++ routes/routes.go
@@ -71,7 +71,6 @@ func AuthCallbackHandler() {
 		session.Save(req, res)
 
 		http.Redirect(res, req, "/", http.StatusTemporaryRedirect)
-		fmt.Printf("user info: %s\n", user)
 	})
 
 	p.Get("/logout/{provider}", func(res http.ResponseWriter, req *http.Request) {
@@ -89,14 +88,16 @@ func AuthCallbackHandler() {
 		res.Header().Set("Location", "/")
 		res.WriteHeader(http.StatusTemporaryRedirect)
 	})
-
 	p.Get("/auth/{provider}", func(res http.ResponseWriter, req *http.Request) {
-		if gothUser, err := gothic.CompleteUserAuth(res, req); err == nil {
-			t, _ := template.New("foo").Parse(userTemplate)
-			t.Execute(res, gothUser)
-		} else {
-			gothic.BeginAuthHandler(res, req)
+		session, _ := store.Get(req, "go-cookie-session-name")
+		if _, loggedIn := session.Values["user_id"]; loggedIn {
+			// If user is already logged in, redirect to the homepage
+			http.Redirect(res, req, "/", http.StatusSeeOther)
+			return
 		}
+
+		// If user is not logged in, initiate the authentication flow
+		gothic.BeginAuthHandler(res, req)
 	})
 
 	p.Get("/", func(res http.ResponseWriter, req *http.Request) {
@@ -124,25 +125,15 @@ type ProviderIndex struct {
 	ProvidersMap map[string]string
 }
 
-var indexTemplate = `{{range $key,$value:=.ProviderIndex.Providers}}
-<p><a href="/auth/{{$value}}">Log in with {{index $.ProviderIndex.ProvidersMap $value}}</a></p>
+var indexTemplate = `
+{{if not .LoggedIn}}
+  {{range $key, $value := .ProviderIndex.Providers}}
+    <p><a href="/auth/{{$value}}">Log in with {{index $.ProviderIndex.ProvidersMap $value}}</a></p>
+  {{end}}
+{{else}}
+  {{range $key, $value := .ProviderIndex.Providers}}
+    <p>Access Token: {{$.AccessToken}}</p>
+    <p><a href="/logout/{{$value}}">Logout using {{index $.ProviderIndex.ProvidersMap $value}}</a></p>
+  {{end}}
 {{end}}
-{{if .LoggedIn}}
-<p>Access Token, {{.AccessToken}}!</p>
-<p><a href="/logout/github">Logout</a></p>
-{{end}}
-`
-
-var userTemplate = `
-<p><a href="/logout/{{.Provider}}">logout</a></p>
-<p>Name: {{.Name}} [{{.LastName}}, {{.FirstName}}]</p>
-<p>Email: {{.Email}}</p>
-<p>NickName: {{.NickName}}</p>
-<p>Location: {{.Location}}</p>
-<p>AvatarURL: {{.AvatarURL}} <img src="{{.AvatarURL}}"></p>
-<p>Description: {{.Description}}</p>
-<p>UserID: {{.UserID}}</p>
-<p>AccessToken: {{.AccessToken}}</p>
-<p>ExpiresAt: {{.ExpiresAt}}</p>
-<p>RefreshToken: {{.RefreshToken}}</p>
 `
